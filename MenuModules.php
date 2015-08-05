c0f3fdd3ba9da8a07a05849b41c40ccbf786ac8e
Vadim Mazur <mazurva@gmail.com>
2015-08-05 15:12:30 +0600

    first commit

---
+++ MenuModules.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vadim
+ * Date: 05.08.15
+ * Time: 14:58
+ */
+
+namespace sibds\helpers;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+
+
+class MenuModules
+{
+
+}
